8af1883bfabff5a2862be214127a1e7838935cb8
Jack Brayan <122466921+jackbrayan17@users.noreply.github.com>
2025-07-03 10:46:14 +0100

    Update settings.py

--- zevaba/settings.py
+++ zevaba/settings.py
@@ -72,9 +72,13 @@ USE_TZ = True
 # Media settings
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
